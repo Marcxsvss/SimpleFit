a0d238ac211c569b775f9aa4b893d5b186aca9be
Marcxsvss <90453251+Marcxsvss@users.noreply.github.com>
2024-03-12 19:51:26 +0100

    Completar AppModules Retrofit

---
+++ SimpleFit_AndroidApp/app/src/main/java/com/simplefit/data/services/UsuarioService.kt
@@ -0,0 +1,30 @@
+package com.simplefit.data.services
+
+import retrofit2.Response
+import retrofit2.http.*
+
+interface UsuarioService {
+//    @GET("usuarios")
+//    @Headers("Accept: application/json", "Content-Type: application/json")
+//    suspend fun usuarios(): Response<List<UsuarioApi>>
+
+    @GET("usuarios/{email}")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun usuario(@Path("email") email: String): Response<UsuarioApi>
+
+//    @GET("contactos/count")
+//    @Headers("Accept: application/json", "Content-Type: application/json")
+//    suspend fun count(): Response<TotalRegistrosApi>
+
+    @POST("usuario")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun insert(@Body u : UsuarioApi): Response<RespuestaApi>
+
+    @PUT("usuarios/{email}")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun update(@Path("email") email: String, @Body c : UsuarioApi): Response<RespuestaApi>
+
+    @DELETE("usuarios/{email}")
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    suspend fun delete(@Path("id") email: String): Response<RespuestaApi>
+}
--- SimpleFit_AndroidApp/app/src/main/java/com/simplefit/di/AppModule.kt
+++ SimpleFit_AndroidApp/app/src/main/java/com/simplefit/di/AppModule.kt
@@ -8,24 +8,52 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
 
-
     @Provides
     @Singleton
-    fun provideUserDatabase(
-        @ApplicationContext context: Context
-    ) : SimpleFitDB = SimpleFitDB.getDatabase(context)
+    fun provideOkHttpClient() : OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.HEADERS
 
+        val timeout = 10L
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(timeout, TimeUnit.SECONDS)
+            .readTimeout(timeout, TimeUnit.SECONDS)
+            .writeTimeout(timeout, TimeUnit.SECONDS)
+            .build()
+    }
     @Provides
     @Singleton
-    fun provideUsersDao(
-        // La Bd es inyectada por provideAgendaDatabase() y
-        // resuelto automáticamente por Hilt
-        db: SimpleFitDB
-    ) : UsuarioDao = db.usuarioDao()
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ) : Retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl("http://10.0.2.2/simplefit/") //Todavia no creado y añadido a tomcat y phpmyadmin
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+//    @Provides
+//    @Singleton
+//    fun provideUserDatabase(
+//        @ApplicationContext context: Context
+//    ) : SimpleFitDB = SimpleFitDB.getDatabase(context)
+//
+//    @Provides
+//    @Singleton
+//    fun provideUsersDao(
+//        // La Bd es inyectada por provideAgendaDatabase() y
+//        // resuelto automáticamente por Hilt
+//        db: SimpleFitDB
+//    ) : UsuarioDao = db.usuarioDao()
 }
